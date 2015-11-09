d3b12010180235042da7fcc817302e5d5258de9b
mgtang28 <miket916@yahoo.com>
2015-11-08 16:48:20 -0800

    Hello World

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // f5 to run
+            Console.Read(); // enter after f5 to get out
+        }
+    }
+}
